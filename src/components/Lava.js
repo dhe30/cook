342b1be6692bb95b9778b3641dcdd1ca9b24abad
Henry Shannon <hshannon30@stuy.edu>
2022-04-24 02:08:35 -0400

    front end makes me cry

--- src/components/Lava.js
+++ src/components/Lava.js
@@ -1,12 +1,25 @@
 import { Container } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
 
 function Lava(props){
-    
     return (
         <Container>
             {props.recipes.map((recs) => {
                 return (
-                <h1>{recs.recipe.label}</h1>)
+                    <h1>
+                        <div>
+                            {recs.recipe.label} 
+                        </div>
+
+                        <Button onClick={props.onClick}>
+                            <img src = {recs.recipe.image}/>
+                        </Button>
+
+                        <li>
+                            {Math.round(recs.recipe.calories)} calories
+                        </li>
+                    </h1>
+                )
             })}
         </Container>
     )
