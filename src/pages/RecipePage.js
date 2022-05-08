3b51cd4548c5d5574dd1ffb32b097bd4e096ba9d
Samuel Chen <schen30@stuy.edu>
2022-05-08 19:19:37 -0400

    ignore

--- src/pages/RecipePage.js
+++ src/pages/RecipePage.js
@@ -10,7 +10,7 @@ function RecipePage(){
                 <Header id = "noGradient"/>
             </Row>
             <Row>
-                <Beans/>
+                <Beans/> 
             </Row>
         </Container>
     )   
