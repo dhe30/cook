92ae090ff13b3fa168d45f7c06b73ada3a84cd38
Samuel Chen <schen30@stuy.edu>
2022-05-08 22:16:53 -0400

    add a div

--- src/components/Lava.js
+++ src/components/Lava.js
@@ -1,23 +1,25 @@
 import { Container, Table } from "react-bootstrap";
-import Button from "react-bootstrap/Button";
-import {useNavigate } from 'react-router-dom';
-import { Row, Col } from 'react-bootstrap';
+// import Button from "react-bootstrap/Button";
+// import {useNavigate } from 'react-router-dom';
+// import { Row, Col } from 'react-bootstrap';
 import RecipeCards from "./cards/RecipeCards";
 import "../index.css";
 
-function Lava(props){
+function Lava(props){ 
     return (
-        <Container class ="cards">
+        <Container>
+            <div class="cards">
             {props.recipes.map((recs) => {
                 return (
                     <RecipeCards image = {recs.recipe.image}
-                                    label = {recs.recipe.label}
-                                    cal = {recs.recipe.calories}
-                                    prepTime = {recs.recipe.totalTime}
-                                    cuisine = {recs.recipe.cuisineType}>
+                                 label = {recs.recipe.label}
+                                 cal = {recs.recipe.calories}
+                                 prepTime = {recs.recipe.totalTime}
+                                 cuisine = {recs.recipe.cuisineType}>
                     </RecipeCards> 
                 )
             })}
+            </div>
         </Container>
     )
 }
