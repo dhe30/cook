d61acb488c922b383e33bd6070209baed19bd802
Samuel Chen <schen30@stuy.edu>
2022-05-14 23:05:03 -0400

    made everything an accordion with checkboxes, need to figure out a way to get rid of white space

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -8,6 +8,7 @@ import '../index.css'
 import { useState, useContext } from 'react';
 import { QueryContextProvider } from '../store/Beans-context';
 import QueryContext from '../store/Beans-context';
+import {Accordion} from 'react-bootstrap';
 function Sidebar(props){
 
   const [typed, setTyped] = useState('');
@@ -81,20 +82,84 @@ function Sidebar(props){
         </div>
       </Row>
       <Row>
-      <ul>
-        <li id="list">
-             Allergies
-        </li>
-
-        <li id="list2">
-            Cuisine Style
-        </li>
-
-        <li id="list3">
-            Dietary Restrictions
-        </li>
-
-      </ul>
+      <div class = "py-3">
+      <Accordion>
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>Allergies</Accordion.Header>
+          <Accordion.Body>
+          <form>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">Crustcean</label></div>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">Dairy</label></div>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">Fish</label></div>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">Mollusk</label></div>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">Peanuts</label></div>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">Soy</label></div>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">Wheat</label></div>
+          </form>
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+      </div>
+      </Row>
+      <Row>
+      <div class = "py-3">
+      <Accordion>
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>Cuisine Style</Accordion.Header>
+          <Accordion.Body>
+          <form>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">American</label></div>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">Asian</label></div>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">British</label></div>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">Carribean</label></div>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">Central Europe</label></div>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">Chinese</label></div>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">Eastern Europe</label></div>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">French</label></div>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">Indian</label></div>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">Italian</label></div>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">Japanese</label></div>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">Kosher</label></div>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">Mediterranean</label></div>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">Mexican</label></div>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">Middle Eastern</label></div>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">Nordic</label></div>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">South American</label></div>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">South East Asian</label></div>
+          </form>
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+      </div>
+      </Row>
+      <Row>
+      <div class = "py-3">
+      <Accordion>
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>Dietary Restrictions</Accordion.Header>
+          <Accordion.Body>
+          <form>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">Balanced</label></div>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">High Fiber</label></div>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">High Protein</label></div>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">Low Carb</label></div>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">Low Fat</label></div>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">Low Sodium</label></div>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">Keto Friendly</label></div>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">Kidney Friendly</label></div>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">No Oil Added</label></div>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">Paleo</label></div>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">Pescatarian</label></div>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">Pork Free</label></div>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">Red Meat Free</label></div>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">Sugar Conscious</label></div>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">Vegan</label></div>
+            <div class="check"><input type= "checkbox"/><label class="checkbox">Vegetarian</label></div>
+          </form>
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+      </div>
       </Row>
       {/*<h1>Love</h1>
       <input type="text" id="search" placeholder='Search...'/>
