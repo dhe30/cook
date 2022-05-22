66dcabc99104ebdfb5328ba233f77dd044943906
Daniel He <dhe30@stuy.edu>
2022-05-21 23:55:44 -0400

    hope

--- src/components/cards/RecipeCards.js
+++ src/components/cards/RecipeCards.js
@@ -5,11 +5,55 @@ import { Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Clock from '../../assets/clock.png';
 import "../../index.css";
+import styles from "./Cards.module.css";
+import { useState } from "react";
 
 
 function RecipeCards(props) {
+    const [love, setLove] = useState("");
     const navigate = useNavigate();
-
+    function fueted(la){
+        switch(la){
+            case "american":
+                return "america";
+            case "asian":
+                return "asia";
+            case "british":
+                return "britain";
+            case "caribbean":
+                return "carribea";
+            case "central europe":
+                return "centralEurope";
+            case "chinese":
+                return "china";
+            case "eastern europe":
+                return "eastEurope";
+            case "french":
+                return "france";
+            case "indian":
+                return "india";
+            case "italian":
+                return "italy";
+            case "japanese":
+                return "japan";
+            case "kosher":
+                return "kosher";
+            case "mediterranean":
+                return "mediterrania";
+            case "mexican":
+                return "mexico";
+            case "middle eastern":
+                return "middleEast";
+            case "nordic":
+                return "nord";
+            case "south american":
+                return "southAmerica";
+            case "south east asian":
+                return "southEastAsia";
+            case "world":
+                return "world";
+        }
+    }
     function addNewRecipeInstruction(egg) {
         console.log(egg);      
     }
@@ -40,18 +84,20 @@ function RecipeCards(props) {
                     
                     
                 </Row>
-                <Row>
+                <Row id = "cardica" >
+                    Cuisine:
                 {props.cuisine.map((type) => {
+                    console.log("AAAAAAAAAAAAAA" + love)
                             return (
-                                <li>
+                                
+                                <Col>
+                                <div id ="lover" className={fueted(type)}>
                                     {type} 
-                                </li>
+                                </div>
+                                </Col>
                             )
                         })}
 
-                        <li>
-                            {props.prepTime} minutes
-                        </li>
                 </Row>
             </Container>
 
