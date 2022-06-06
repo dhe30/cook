7afa0bc8b38a241acb054e1590959698f80e5857
Henry Shannon <hshannon30@stuy.edu>
2022-06-05 20:27:07 -0400

    Making nextpage a class rn

--- src/components/NextPage.js
+++ src/components/NextPage.js
@@ -5,61 +5,62 @@ import { Container, Row } from "react-bootstrap";
 import  Bars from '../assets/bar.gif';
 import { render } from '@testing-library/react';
 
+let newLink;
+let resultName;
+
 class NextPage extends React.component {
     constructor(props){
         super(props);
         console.log(this.props.link);
     }
+    
+    componentDidMount() {
+        newLink = props.link;
+        resultName = props.typed;
 
-    render() {
-        let newLink = props.link;
-        let resultName = props.typed;
-        
-        
         const [nextRecipes, setNextRecipes] = useState([]);
-        
-        componentDidMount() {
-            console.log("fetching new data")
-            fetch(newLink)
-                    .then((res) => res.json())
-                    .then((data) => {
-                        console.log(data);
-                        setNextRecipes({repos: data.hits});
-                        newLink = data._links.next.href;
-                        console.log(data);
-                    }).catch(err => {
-                        console.log("fat error");
-                    });
-        };  
 
-        function goNext() {;
-            return (
-                <div>
-                    <Morbius morb = {nextRecipes} query = {resultName}/>
-                </div>  
-            )
-        }
+        console.log("fetching new data")
+        fetch(newLink)
+                .then((res) => res.json())
+                .then((data) => {
+                    console.log(data);
+                    setNextRecipes({repos: data.hits});
+                    newLink = data._links.next.href;
+                    console.log(data);
+                }).catch(err => {
+                    console.log("fat error");
+                });
+    };  
 
-        if (nextRecipes === undefined || nextRecipes === null){
-            return(
-                <Container fluid className= "Loading">
-                
-                <Row className = "Loading2">
-                    <div>
-                        LOADING...
-                        <img  className = "LoadingBars" src={Bars}/>
-                    </div>
-                </Row>
-            </Container>
-            )
-        }
-
+    function goNext() {;
         return (
-            <Button onClick = {() => goNext()}>
-                20 More :D
-            </Button>
+            <div>
+                <Morbius morb = {nextRecipes} query = {resultName}/>
+            </div>  
         )
     }
+
+    if (nextRecipes === undefined || nextRecipes === null){
+        return(
+            <Container fluid className= "Loading">
+            
+            <Row className = "Loading2">
+                <div>
+                    LOADING...
+                    <img  className = "LoadingBars" src={Bars}/>
+                </div>
+            </Row>
+        </Container>
+        )
+    }
+
+    return (
+        <Button onClick = {() => goNext()}>
+            20 More :D
+        </Button>
+    )
+    
 }
 
 export default NextPage;
